591b7732cef8bd4af83760e5608b86c010516af0
Adones Pitogo <pitogo.adones@gmail.com>
2023-05-20 19:11:40 +0800

    palm reject top 15mm

--- src/evdev-mt-touchpad.c
+++ src/evdev-mt-touchpad.c
@@ -3372,7 +3372,7 @@ tp_init_palmdetect_edge(struct tp_dispatch *tp,
 
 	if (!tp->buttons.has_topbuttons && height > 55) {
 		/* top edge is 5% of the height */
-		mm.y = height * 0.1;
+		mm.y = 15;
 		edges = evdev_device_mm_to_units(device, &mm);
 		tp->palm.upper_edge = edges.y;
 	}
